64ebf0e17e7c33ef44155e8c6574143ecb93e89f
Lucas Henrique Paes de Carvalho <lukas.paes18@gmail.com>
2021-09-30 15:15:27 -0300

    build to github pages

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,7 @@
 import Cyberpunk from 'assets/images/background.png'
 
+import Logo03 from 'assets/images/logo-03.jpeg'
+
 export const Home = () => {
   return (
     <div
@@ -8,10 +10,13 @@ export const Home = () => {
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        background: '#FCD594',
         width: '100%',
       }}
     >
+      <div style={{ width: '50%', height: '40%' }}>
+        <img src={Logo03} alt="logo" width="100%" height="100%" />
+      </div>
+
       <img src={Cyberpunk} alt="logo" />
     </div>
   )
